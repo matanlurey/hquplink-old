c8ab8809338a3555330b2ccaa4362ecf0b1326a3
Matan Lurey <matanlurey@users.noreply.github.com>
2018-11-23 16:23:06 -0800

    Add a bit of a shell (#6)
    * Start on shell of an app.
    
    * List core units and weapons.
    
    * Add new pages.

---
+++ app/lib/src/app.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import 'ui/pages/browse.dart' as pages;
+import 'ui/scaffold.dart';
+
+/// Root [Widget] for the `HQ Uplink` application.
+class HQUplinkApp extends StatefulWidget {
+  const HQUplinkApp();
+
+  @override
+  createState() => _HQUplinkAppState();
+}
+
+class _HQUplinkAppState extends State<HQUplinkApp> {
+  var _activeIndex = 0;
+
+  @override
+  build(BuildContext context) {
+    // Determine the active page, by index of selected bottom navigation button.
+    final allPages = <HQUplinkPage>[
+      HQUplinkPage(
+        title: 'Home',
+        route: '/',
+        build: (_) => Center(child: Text('Home')),
+      ),
+      HQUplinkPage(
+        title: 'Browse',
+        route: '/browse',
+        build: (_) => pages.BrowsePage(),
+      ),
+      HQUplinkPage(
+        title: 'Profile',
+        route: '/profile',
+        build: (_) => Center(child: Text('Profile')),
+      ),
+    ];
+    return MaterialApp(
+      title: 'HQ Uplink',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      routes: Map<String, WidgetBuilder>.fromIterable(allPages,
+          key: (p) => p.route,
+          value: (p) {
+            return (context) {
+              return HQUplinkScaffold(
+                activeBottomNav: _activeIndex,
+                onBottomNavTapped: (index) {
+                  setState(() {
+                    _activeIndex = index;
+                  });
+                  switch (index) {
+                    case 0:
+                      return Navigator.pushReplacementNamed(context, '/');
+                    case 1:
+                      return Navigator.pushReplacementNamed(context, '/browse');
+                    case 2:
+                      return Navigator.pushReplacementNamed(
+                          context, '/profile');
+                  }
+                },
+                page: allPages[_activeIndex],
+              );
+            };
+          }),
+    );
+  }
+}
